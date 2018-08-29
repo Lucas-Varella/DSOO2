3b06b22195105446528b7a7650618352559f96be
marcoslaydner <marcoslaydner@cliente-150-162-182-112.wireless.ufsc.br>
2018-08-29 17:16:38 -0300

    Mexendo nas coisa

--- TrabalhoDSOO1/src/br/ufsc/Trabalho1/model/Employee.java
+++ TrabalhoDSOO1/src/br/ufsc/Trabalho1/model/Employee.java
@@ -6,11 +6,49 @@ public class Employee extends User{
 	
 	private String position;
 	private int salary;
+	private String username;
+	private String password;
 
-	public Employee(String cpf, String rg, String name, Date birthday, String address, String position, int salary) {
+	public Employee(String cpf, String rg, String name, Date birthday, String address, 
+			String position, int salary, String username, String password) {
+		
 		super(cpf, rg, name, birthday, address);
-		this.salary = salary;
+		this.setSalary(salary);
+		this.setPosition(position);
+		this.password = password;
+		this.username = username;
+	}
+
+	public String getPosition() {
+		return position;
+	}
+
+	public void setPosition(String position) {
 		this.position = position;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
