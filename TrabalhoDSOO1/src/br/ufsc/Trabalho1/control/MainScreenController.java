58c57f41d70b37fec0cff0c67dcfb314a308f5c5
marcoslaydner <marcoslaydner@cliente-150-162-182-112.wireless.ufsc.br>
2018-08-29 18:01:49 -0300

    sei la bixo

---
+++ TrabalhoDSOO1/src/br/ufsc/Trabalho1/control/MainScreenController.java
@@ -0,0 +1,5 @@
+package br.ufsc.Trabalho1.control;
+
+public class MainScreenController {
+
+}
--- TrabalhoDSOO1/src/br/ufsc/Trabalho1/model/Trainee.java
+++ TrabalhoDSOO1/src/br/ufsc/Trabalho1/model/Trainee.java
@@ -1,11 +1,24 @@
 package br.ufsc.Trabalho1.model;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Trainee extends User{
 
+	private List<String> train;
+	
 	public Trainee(String cpf, String rg, String name, Date birthday, String address) {
 		super(cpf, rg, name, birthday, address);
+		this.setTrain(new ArrayList());
+	}
+
+	public List<String> getTrain() {
+		return train;
+	}
+
+	public void setTrain(List<String> train) {
+		this.train = train;
 	}
 
 }
